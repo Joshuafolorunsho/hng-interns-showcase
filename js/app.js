ca4848a7be81b6656980cbae9e9f903f7b83763f
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2019-11-01 13:59:39 +0100

    see more button now functional

--- js/app.js
+++ js/app.js
@@ -1,67 +1,24 @@
 (function() {
 	const showMoreBtn = document.querySelector('#showmore');
+	const more = document.querySelector('.more');
+	let state = false;
+	//listening for a click event on showmore button
 	showMoreBtn.addEventListener('click', function() {
-		const displayNone = document.querySelector('.togglenone');
-		if (showMoreBtn.textContent == 'Show More') {
-			displayNone.classList.toggle('togglenone');
-			showMore.textContent = 'Show Less';
+		console.log(state);
+		if (!state) {
+			more.style.display = 'block';
+			showMoreBtn.textContent = 'Show Less';
+			state = true;
+		} else {
+			more.style.display = 'none';
+			showMoreBtn.textContent = 'Show More';
+			state = false;
 		}
-		// else {
-		// 	displayNone.classList.toggle('togglenone');
-		// 	displayNone.className = 'togglenone';
-		// 	showMore.textContent = 'Show More';
-		// }
 	});
 })();
 
+// Toggle menu function
 function toggleMenu() {
 	let menuBar = $('#navMenu');
 	menuBar.toggle();
 }
-
-function grabHeight() {
-	var D = document;
-	return Math.max(
-		D.body.scrollHeight,
-		D.documentElement.scrollHeight,
-		D.body.offsetHeight,
-		D.documentElement.offsetHeight,
-		D.body.clientHeight,
-		D.documentElement.clientHeight
-	);
-}
-
-let prev = 0;
-function mainfunc() {
-	//prev = pctScrolled;
-	var winheight =
-		window.innerHeight ||
-		(document.documentElement || document.body).clientHeight;
-	var docheight = grabHeight();
-	var scrollTop =
-		window.pageYOffset ||
-		(document.documentElement || document.body.parentNode || document.body)
-			.scrollTop;
-	var trackLength = docheight - winheight;
-	var pctScrolled = (scrollTop / trackLength) * 100;
-
-	if (scrollTop > prev) {
-		document.getElementById('header').classList.add('navbg2');
-		document.getElementById('header').classList.remove('navbg');
-		document.getElementById('header').classList.remove('dynamic');
-	} else {
-		document.getElementById('header').classList.add('navbg');
-		document.getElementById('header').classList.remove('navbg2');
-		document.getElementById('header').classList.add('dynamic');
-	}
-	if (pctScrolled < 0.3) {
-		document.getElementById('header').classList.add('navbg2');
-		document.getElementById('header').classList.remove('navbg');
-		document.getElementById('header').classList.remove('dynamic');
-	}
-
-	return (prev = scrollTop <= 0 ? 0 : scrollTop);
-}
-window.addEventListener('scroll', function() {
-	mainfunc();
-});
